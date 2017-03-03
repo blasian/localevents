9d870e68c52c8a9fb543daf20136741034db801e
Michael Spearman <mikespearman.e@gmail.com>
2017-03-03 06:19:52 -0800

    Initial Commit

---
+++ LocalEvents/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocalEvents
+//
+//  Created by Michael Spearman on 3/3/17.
+//  Copyright © 2017 Blasian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocalEvents/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocalEvents
+//
+//  Created by Michael Spearman on 3/3/17.
+//  Copyright © 2017 Blasian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
